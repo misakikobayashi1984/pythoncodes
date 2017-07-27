cffb5959d98005bbd7948c47fc597b7955e0d5f2
misakikobayashi1984 <misaki.kobayashi1984@gmail.com>
2017-07-28 06:40:33 +0900

    add file related to klf

--- py_sim/kalman_filter_r2.py
+++ py_sim/kalman_filter_r2.py
@@ -18,6 +18,7 @@ Created on Tue Jul 25 16:30:24 2017
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+%matplotlib inline 
 
 def lkf(T, Y, U, mu0, Sigma0, A, B, C, Q, R):
     '''Linear Kalman Filter
@@ -62,55 +63,59 @@ def lkf(T, Y, U, mu0, Sigma0, A, B, C, Q, R):
     return M
 
 ''' 設定 '''
-vlc = 30 #[kph]     
-x_0 = np.mat([[-80],[1.6]]) #[m]
-del_ts = 0.1 #[s]
-T_s = 10 #[s]
-
-''' 定数 '''
-vlc_mps = vlc / 3.6
-T = int( T_s / del_ts )# 観測数    
-
-# 状態方程式
-# x = A * x_ + B * u + w, w ~ N(0,Q)
-A = np.mat([[1,0], [0,1]])
-B = np.mat([[del_ts,0], [0,del_ts]])
-Q = np.mat([[0.001,0], [0,0.001]])#真値に乗る誤差
-u = np.mat([[vlc_mps],[0]]) # 入力（一定）
-# 観測方程式
-# y = C * x + v, v ~ N(0,R)
-C = np.mat([[1,0], [0,1]])#単位変換のための行列
-R = np.mat([[0.2,0], [0,0.2]])#観測値に乗る誤差
-
-# 観測のテストデータの生成
-x = x_0 # 初期位置
-X = [x] # 状態列
-Y = [x] # 観測列
-U = [u] # 入力列
-for i in range(T):
-    x = A * x + B * u + np.random.multivariate_normal([0, 0], Q, 1).T
-    X.append(x)
-    y = C * x + np.random.multivariate_normal([0, 0], R, 1).T
-    Y.append(y)
-    U.append(u)
-
-# LKF
-mu0 = x_0 # 初期状態推定値
-Sigma0 = np.mat([[0,0],[0,0]]) # 初期誤差共分散行列
-M = lkf(T, Y, U, mu0, Sigma0, A, B, C, Q, R)
-
-# 描画
-fig = plt.figure()
-ax = fig.add_subplot(111)
-a, b = np.array(np.concatenate(X,axis=1))
-ax.plot(a,b,color='red',marker='o',label='gt')
-a, b = np.array(np.concatenate(Y,axis=1))
-ax.plot(a,b,color='blue',marker='o',label='obseved')
-a, b = np.array(np.concatenate(M,axis=1))
-ax.plot(a,b,color='orange',marker='o',label='lkf output')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.legend(loc="lower right")
+def plot_pos():
+    vlc = 30 #[kph]     
+    x_0 = np.mat([[-80],[1.6]]) #[m]
+    del_ts = 0.1 #[s]
+    T_s = 10 #[s]
+    
+    ''' 定数 '''
+    vlc_mps = vlc / 3.6
+    T = int( T_s / del_ts )# 観測数
+    
+    # 状態方程式
+    # x = A * x_ + B * u + w, w ~ N(0,Q)
+    A = np.mat([[1,0], [0,1]])
+    B = np.mat([[del_ts,0], [0,del_ts]])
+    Q = np.mat([[0.001,0], [0,0.001]])#真値に乗る誤差
+    u = np.mat([[vlc_mps],[0]]) # 入力（一定）
+    # 観測方程式
+    # y = C * x + v, v ~ N(0,R)
+    C = np.mat([[1,0], [0,1]])#単位変換のための行列
+    R = np.mat([[0.2,0], [0,0.2]])#観測値に乗る誤差
+    
+    # 観測のテストデータの生成
+    x = x_0 # 初期位置
+    X = [x] # 状態列
+    Y = [x] # 観測列
+    U = [u] # 入力列
+    for i in range(T):
+        x = A * x + B * u + np.random.multivariate_normal([0, 0], Q, 1).T
+        X.append(x)
+        y = C * x + np.random.multivariate_normal([0, 0], R, 1).T
+        Y.append(y)
+        U.append(u)
+    
+    # LKF
+    mu0 = x_0 # 初期状態推定値
+    Sigma0 = np.mat([[0,0],[0,0]]) # 初期誤差共分散行列
+    M = lkf(T, Y, U, mu0, Sigma0, A, B, C, Q, R)
+    
+    # 描画
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    a, b = np.array(np.concatenate(X,axis=1))
+    ax.plot(a,b,color='red',marker='o',label='gt')
+    a, b = np.array(np.concatenate(Y,axis=1))
+    ax.plot(a,b,color='blue',marker='o',label='obseved')
+    a, b = np.array(np.concatenate(M,axis=1))
+    ax.plot(a,b,color='orange',marker='o',label='lkf output')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.legend(loc="lower right")
+    
+if __name__ == "__main__":
+    plot_pos()
 
 #例
 # Q = np.mat([[0,0], [0,0]])の時
